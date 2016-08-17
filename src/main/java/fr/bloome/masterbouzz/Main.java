bba57570838838c1327c4a38b5e9fea4cdee3747
louisld <louisld@users.noreply.github.com>
2016-08-17 21:32:33 +0200

    Ok

---
+++ src/main/java/fr/bloome/masterbouzz/Main.java
@@ -0,0 +1,9 @@
+package fr.bloome.masterbouzz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/main/java/fr/bloome/masterbouzz/MainWindow.java
@@ -0,0 +1,12 @@
+package fr.bloome.masterbouzz;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+	MainWindow(){
+		this.setTitle("Maserbouzz");
+		this.setSize(250, 300);
+		this.setResizable(true);
+		this.setVisible(true);
+	}
+}
